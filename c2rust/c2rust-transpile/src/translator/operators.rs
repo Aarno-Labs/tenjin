Aarno-Labs/tenjin

commit b15ca9847b2eb5682cc41f62ecd6bf38c665b79f

wip

c2rust/c2rust-transpile/src/translator/operators.rs
@@ -249,7 +249,19 @@ impl Translation<'_> {
             .kind
             .get_qual_type()
             .ok_or_else(|| format_err!("bad assignment rhs type"))?;
-        let rhs_translation = self.convert_expr(ctx.used(), rhs)?;
+
+        if let Some(tres) =
+            self.recognize_c_assignment_cases(ctx, op, qtype, lhs, rhs, compute_type, result_type)?
+        {
+            if ctx.is_unused() && tres.is_pure() {
+                return Ok(WithStmts::new(
+                    vec![mk().semi_stmt(tres.to_expr())],
+                    mk().tuple_expr(vec![]),
+                ));
+            }
+        }
+
+        let rhs_translation = self.convert_expr(ctx, rhs)?;
         self.convert_assignment_operator_with_rhs(
             ctx,
             op,

c2rust/c2rust-ast-builder/src/builder.rs
@@ -856,7 +856,7 @@ impl Builder {
         // If the expression is itself a cast of an integer literal, and the
         // inner cast does not change the value, we can omit the inner cast.
         if let Expr::Cast(inner) = &*e {
-            if tenjin::int_lit_cast_redundant(&inner.expr, &inner.ty) {
+            if tenjin::int_lit_cast_never_truncates(&inner.expr, &inner.ty) {
                 // If the inner cast is redundant, we can just return the inner expression.
                 return Box::new(parenthesize_if_necessary(Expr::Cast(ExprCast {
                     attrs: self.attrs,
@@ -2402,7 +2402,7 @@ fn parenthesize_if_necessary(mut outer: Expr) -> Expr {
 mod tenjin {
     use super::*;
 
-    pub fn int_lit_cast_redundant(e: &Expr, t: &Type) -> bool {
+    pub fn int_lit_cast_never_truncates(e: &Expr, t: &Type) -> bool {
         if let Expr::Lit(lit) = e {
             match &lit.lit {
                 Lit::Char(clit) => {
@@ -2448,6 +2448,8 @@ mod tenjin {
             // representable by the given C type, that's not so good; we
             // may end up generating incorrect code due to the missing cast,
             // depending on the situation.
+            //
+            // So these values are chosen conservatively.
             if path.segments[1].ident.to_string().as_str() == "c_ulong" {
                 return Some(i32::MAX as i64);
             }
@@ -2459,7 +2461,7 @@ mod tenjin {
             }
         }
         match path.segments[0].ident.to_string().as_str() {
-            "u8" | "i8" | "bool" | "char" => Some(i8::MAX as i64),
+            "u8" | "i8" | "char" => Some(i8::MAX as i64),
             "u16" | "i16" => Some(i16::MAX as i64),
             "u32" | "i32" => Some(i32::MAX as i64),
             "u64" | "i64" => Some(i64::MAX),

c2rust/c2rust-transpile/src/c_ast/mod.rs
@@ -57,6 +57,8 @@ pub struct TypedAstContext {
     pub c_main: Option<CDeclId>,
     pub parents: HashMap<CDeclId, CDeclId>, // record fields and enum constants
 
+    pub parent_fn: HashMap<CDeclId, CDeclId>, // decl => parent function
+
     // Mapping from FileId to SrcFile. Deduplicated by file path.
     files: Vec<SrcFile>,
     // Mapping from clang file id to translator FileId

c2rust/c2rust-transpile/src/lib.rs
@@ -83,6 +83,8 @@ pub struct TranspilerConfig {
     pub preserve_unused_functions: bool,
     pub log_level: log::LevelFilter,
 
+    pub guidance_json: serde_json::Value,
+
     // Options that control build files
     /// Emit `Cargo.toml` and `lib.rs`
     pub emit_build_files: bool,

c2rust/c2rust-transpile/src/translator/literals.rs
@@ -138,6 +138,19 @@ impl Translation<'_> {
                     // zero terminator
                     _ => 1,
                 };
+
+                log::info!(
+                    "TENJIN TRACE: convert string literal, contents len {}, num elems {}",
+                    val.len(),
+                    num_elems
+                );
+
+                // XREF:TENJIN-GUIDANCE-STRAWMAN
+                if num_elems == 100 && val.is_empty() {
+                    let newstr = mk().call_expr(mk().path_expr(vec!["String", "new"]), vec![]);
+                    return Ok(WithStmts::new_val(newstr));
+                }
+
                 let size = num_elems * (width as usize);
                 val.resize(size, 0);
 

c2rust/c2rust-transpile/src/translator/tenjin.rs
@@ -1,0 +1,219 @@
+use super::*;
+use syn::{Expr, Path, Type};
+
+pub fn is_known_size_1_type(ty: &Type) -> bool {
+    match ty {
+        Type::Path(path) => path.qself.is_none() && is_known_size_1_path(&path.path),
+        _ => false,
+    }
+}
+
+fn is_path_exactly_1(path: &Path, a: &str) -> bool {
+    if path.segments.len() == 1 {
+        path.segments[0].ident.to_string().as_str() == a
+    } else {
+        false
+    }
+}
+
+fn is_path_exactly_2(path: &Path, a: &str, b: &str) -> bool {
+    if path.segments.len() == 2 {
+        path.segments[0].ident.to_string().as_str() == a
+            && path.segments[1].ident.to_string().as_str() == b
+    } else {
+        false
+    }
+}
+
+fn is_known_size_1_path(path: &Path) -> bool {
+    match path.segments.len() {
+        1 => matches!(
+            path.segments[0].ident.to_string().as_str(),
+            "u8" | "i8" | "bool" | "char"
+        ),
+        2 => is_path_exactly_2(path, "libc", "c_char"),
+        _ => false,
+    }
+}
+
+pub fn expr_is_ident(expr: &Expr, ident: &str) -> bool {
+    if let Expr::Path(ref path) = *expr {
+        is_path_exactly_1(&path.path, ident)
+    } else {
+        false
+    }
+}
+
+/// The given expression is being used in a context expecting a u64 value.
+/// Builder::cast_expr() will strip value-preserving casts. Because we know
+/// the type imposed by the context, we can also entirely elide casts of
+/// compatible integer literals.
+/// Examples with expr = x of type i16, and with the implicit `as u64` written explicitly:
+///      (x as i8)    as u64 => (x as i8) as u64     # inner cast may change value
+///      (x as i32)   as u64 => x as u64             # inner cast is value-preserving
+///      (1000 as i8) as u64 => (1000 as i8) as u64  # with large literal, inner cast is not value-preserving
+///      (10 as i8)   as u64 => 10                   # with small literal, inner cast is value-preserving
+///      1000 as u64         => 1000                 # outer cast is made redundant by known context
+pub fn expr_in_u64(expr: Box<Expr>) -> Box<Expr> {
+    use crate::translator::mk;
+    let cast_box = mk().cast_expr(expr, mk().path_ty(vec!["u64"]));
+    // If we end up with a cast of a literal, we can elide the outer cast.
+    if let Expr::Cast(ref cast) = *cast_box {
+        if let Expr::Lit(ref elit) = *cast.expr {
+            if let syn::Lit::Int(ref lit_int) = elit.lit {
+                // If the literal is small enough, we can elide the cast
+                if lit_int.base10_parse::<u64>().is_ok() {
+                    return cast.expr.clone();
+                }
+            }
+        }
+    }
+    cast_box
+}
+
+impl Translation<'_> {
+    pub fn recognize_c_assignment_cases(
+        &self,
+        ctx: ExprContext,
+        op: c_ast::BinOp,
+        qtype: CQualTypeId,
+        lhs: CExprId,
+        rhs: CExprId,
+        compute_type: Option<CQualTypeId>,
+        result_type: Option<CQualTypeId>,
+    ) -> TranslationResult<Option<WithStmts<Box<Expr>>>> {
+        self.recognize_c_assignment_string_pop(ctx, op, qtype, lhs, rhs, compute_type, result_type)
+    }
+
+    fn strip_integral_cast(&self, expr: CExprId) -> CExprId {
+        let kind = &self.ast_context.index(expr).kind;
+        if let CExprKind::ImplicitCast(_, inner, CastKind::IntegralCast, _, _) = kind {
+            *inner
+        } else {
+            expr
+        }
+    }
+
+    fn is_integral_lit(&self, expr: CExprId, val: u64) -> bool {
+        let kind = &self.ast_context.index(expr).kind;
+        if let CExprKind::Literal(_, clit) = kind {
+            match clit {
+                CLiteral::Integer(value, _base) => *value == val,
+                CLiteral::Character(value) => *value == val,
+                _ => false,
+            }
+        } else {
+            false
+        }
+    }
+
+    fn c_expr_decl_id(&self, expr: CExprId) -> Option<CDeclId> {
+        let kind = &self
+            .ast_context
+            .index(self.c_strip_implicit_casts(expr))
+            .kind;
+        if let CExprKind::DeclRef(_, decl_id, _) = kind {
+            Some(*decl_id)
+        } else {
+            None
+        }
+    }
+
+    fn recognize_c_assignment_string_pop(
+        &self,
+        ctx: ExprContext,
+        op: c_ast::BinOp,
+        qtype: CQualTypeId,
+        lhs: CExprId,
+        rhs: CExprId,
+        compute_type: Option<CQualTypeId>,
+        result_type: Option<CQualTypeId>,
+    ) -> TranslationResult<Option<WithStmts<Box<Expr>>>> {
+        // Code matching
+        //     s1[strlen(s1)-1] = '\0';
+        //         when s1 is guided to be a String
+        // can be translated to
+        //     s1.pop();
+        // (assuming the assignment expr is in a statement position)
+        log::info!("Recognizing C assignment string pop?...");
+        if op != c_ast::BinOp::Assign {
+            log::info!("Not an assignment operator");
+            return Ok(None);
+        }
+
+        if !self.is_integral_lit(self.strip_integral_cast(rhs), 0) {
+            log::info!("Not assigned an integral literal 0");
+            return Ok(None);
+        }
+
+        let lhs_kind = &self.ast_context.index(lhs).kind;
+        if let CExprKind::ArraySubscript(_, raw_base, index, _lrvalue) = lhs_kind {
+            if let CExprKind::Binary(
+                _sub_cq,
+                c_ast::BinOp::Subtract,
+                sub_lhs,
+                sub_rhs,
+                _opt_cq_lhs,
+                _opt_cq_rhs,
+            ) = self.ast_context.index(*index).kind
+            {
+                if !self.is_integral_lit(self.strip_integral_cast(sub_rhs), 1) {
+                    log::info!("Not subtracting 1 from the index");
+                    return Ok(None);
+                }
+
+                if let Some(base_decl_id) = self.c_expr_decl_id(*raw_base) {
+                    let base_decl = &self.ast_context[base_decl_id].kind;
+                    if let CDeclKind::Variable { ident, .. } = base_decl {
+                        if !(ident == "s1" || ident == "s2") {
+                            log::info!("target string not s1 or s2");
+                            return Ok(None);
+                        }
+                    }
+                } else {
+                    return Ok(None);
+                };
+
+                if let CExprKind::Call(_, callee_raw, args) = &self.ast_context.index(sub_lhs).kind
+                {
+                    let callee = self.c_strip_implicit_casts(*callee_raw);
+                    if let CExprKind::DeclRef(_, callee_decl_id, _opt_cq) =
+                        self.ast_context.index(callee).kind
+                    {
+                        if let CDeclKind::Function { name, .. } =
+                            &self.ast_context[callee_decl_id].kind
+                        {
+                            if name != "strlen" {
+                                return Ok(None);
+                            }
+
+                            if args.len() == 1
+                                && self.c_expr_decl_id(args[0]) == self.c_expr_decl_id(*raw_base)
+                            {
+                                let tgt_expr =
+                                    self.convert_expr(ctx, self.c_strip_implicit_casts(*raw_base))?;
+                                let pop_call = mk().method_call_expr(
+                                    tgt_expr.to_expr(),
+                                    mk().path_segment("pop"),
+                                    vec![],
+                                );
+                                log::info!("Recognized assignment, returning pop call");
+                                return Ok(Some(WithStmts::new_val(pop_call)));
+                            } else {
+                                log::info!("Call to strlen does not match the expected argument");
+                            }
+                        }
+                        log::info!("Not a call to strlen");
+                    } else {
+                        log::info!(
+                            "Callee is not a decl ref: {:?}",
+                            self.ast_context.index(callee).kind
+                        );
+                    }
+                }
+            }
+        }
+        log::info!("LHS not an array subscript");
+        Ok(None)
+    }
+}

c2rust/c2rust/Cargo.toml
@@ -25,6 +25,7 @@ log = "0.4"
 regex = "1.3"
 shlex = "1.3"
 c2rust-transpile = { version = "0.20.0", path = "../c2rust-transpile" }
+serde_json = "1.0"
 
 [build-dependencies]
 c2rust-build-paths = { path = "../c2rust-build-paths", version = "0.20.0" }

commit dbbd7f09aa92fa416cd81f3eaa6a36c12e73820c

Fix bug introduced in b15ca9847b2eb5682cc41f62ecd6bf38c665b79f
yay bisect!

c2rust/c2rust-transpile/src/translator/operators.rs
@@ -262,7 +262,7 @@ impl Translation<'_> {
         }
 
         let lhs_guidance = self.parsed_guidance.borrow_mut().query_expr_type(self, lhs);
-        let rhs_translation = self.convert_expr_guided(ctx, rhs, &lhs_guidance)?;
+        let rhs_translation = self.convert_expr_guided(ctx.used(), rhs, &lhs_guidance)?;
         self.convert_assignment_operator_with_rhs(
             ctx,
             op,