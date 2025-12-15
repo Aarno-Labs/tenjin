ed20efbf19f50935750dc5225489a5cc910636aa
Alexander Bakst <abakst@aarno-labs.com>
2025-12-15 13:38:48 -0800

    add env for vscode

--- cli/testing.py
+++ cli/testing.py
@@ -1,29 +1,35 @@
+from pathlib import Path
 import pytest
 
 import cli_subcommands
 import repo_root
 
+
 @pytest.fixture
-def root():
+def root() -> Path:
     root = repo_root.find_repo_root_dir_Path()
     cli_subcommands.do_build_rs(root)
     return root
 
-@pytest.fixture
-def test_dir(request):
-  return request.path.parent
 
 @pytest.fixture
-def test_tmp_dir(tmp_path):
-  return tmp_path
+def test_dir(request) -> Path:
+    return request.path.parent
+
 
 @pytest.fixture
-def tmp_codebase(test_tmp_dir):
-  codebase = test_tmp_dir / "codebase"
-  return codebase
+def test_tmp_dir(tmp_path) -> Path:
+    return tmp_path
+
 
 @pytest.fixture
-def tmp_resultsdir(test_tmp_dir):
-  resultsdir = test_tmp_dir / "resultsdir"
-  resultsdir.mkdir()
-  return resultsdir
+def tmp_codebase(test_tmp_dir) -> Path:
+    codebase = test_tmp_dir / "codebase"
+    return codebase
+
+
+@pytest.fixture
+def tmp_resultsdir(test_tmp_dir) -> Path:
+    resultsdir = test_tmp_dir / "resultsdir"
+    resultsdir.mkdir()
+    return resultsdir
--- tests/smoke_tests/test_0.py
+++ tests/smoke_tests/test_0.py
@@ -3,6 +3,7 @@ import translation_preparation
 import translation
 import hermetic
 
+
 def run_cargo_on_final(cwd: Path, args: list[str], capture_output: bool = False):
     return hermetic.run_cargo_on_translated_code(
         args,
@@ -11,79 +12,83 @@ def run_cargo_on_final(cwd: Path, args: list[str], capture_output: bool = False)
         capture_output=capture_output,
     )
 
+
 def test_smoketest0(root, test_dir, test_tmp_dir, tmp_codebase, tmp_resultsdir):
-  codebase = test_dir / "test_0" / "main.c"
+    codebase = test_dir / "test_0" / "main.c"
 
-  translation_preparation.copy_codebase(codebase, tmp_codebase)
+    translation_preparation.copy_codebase(codebase, tmp_codebase)
 
-  # Ensure it compiles and runs as expected
-  hermetic.run(
-    ["clang", "-o", str(test_tmp_dir / "main"), str(tmp_codebase / "main.c")], check=True
-  )
-  c_prog_output = hermetic.run([str(test_tmp_dir / "main")], check=True, capture_output=True)
-  assert c_prog_output.stdout == b"Hello, Tenjin!\n"
+    # Ensure it compiles and runs as expected
+    hermetic.run(
+        ["clang", "-o", str(test_tmp_dir / "main"), str(tmp_codebase / "main.c")], check=True
+    )
+    c_prog_output = hermetic.run([str(test_tmp_dir / "main")], check=True, capture_output=True)
+    assert c_prog_output.stdout == b"Hello, Tenjin!\n"
 
-  # Run translation
-  translation.do_translate(
-      root,
-      tmp_codebase,
-      tmp_resultsdir,
-      cratename="smoke_test_0",
-      guidance_path_or_literal="{}",
-  )
+    # Run translation
+    translation.do_translate(
+        root,
+        tmp_codebase,
+        tmp_resultsdir,
+        cratename="smoke_test_0",
+        guidance_path_or_literal="{}",
+    )
 
-  assert (tmp_resultsdir / "final" / "Cargo.toml").exists()
+    assert (tmp_resultsdir / "final" / "Cargo.toml").exists()
 
-  run_cargo_on_final(tmp_resultsdir / "final", ["build"])
-  rs_prog_output = run_cargo_on_final(tmp_resultsdir / "final", ["run"], capture_output=True)
+    run_cargo_on_final(tmp_resultsdir / "final", ["build"])
+    rs_prog_output = run_cargo_on_final(tmp_resultsdir / "final", ["run"], capture_output=True)
 
-  assert rs_prog_output.stdout == c_prog_output.stdout
+    assert rs_prog_output.stdout == c_prog_output.stdout
+
 
 def test_smoketest1(test_dir, tmp_codebase):
-  codebase = test_dir / "test_1" / "main.c"
-  target = tmp_codebase / "main.c"
-  translation_preparation.copy_codebase(codebase, tmp_codebase)
-  hermetic.run_chkc(["c-file", "parse", str(target)], check=True)
-  hermetic.run_chkc(["c-file", "analyze", str(target)], check=True)
-  hermetic.run_chkc(["c-file", "report", str(target)], check=True)
+    codebase = test_dir / "test_1" / "main.c"
+    target = tmp_codebase / "main.c"
+    translation_preparation.copy_codebase(codebase, tmp_codebase)
+    hermetic.run_chkc(["c-file", "parse", str(target)], check=True)
+    hermetic.run_chkc(["c-file", "analyze", str(target)], check=True)
+    hermetic.run_chkc(["c-file", "report", str(target)], check=True)
+
 
 def test_smoketest2(root, test_dir, tmp_codebase, tmp_resultsdir):
-  codebase = test_dir / "test_2"
-  translation_preparation.copy_codebase(codebase, tmp_codebase)
-  # For reasons I don't understand yet, intercept-build sometimes creates an empty
-  # compile_commands.json on macOS.
-  hermetic.run(
-      ["clang", "-c", "a.c", "b.c", "main.c", "-MJ", "compile_commands.json"],
-      cwd=tmp_codebase,
-      check=True,
-  )
-  # -MJ produces a comma-terminated list; fix it to be valid JSON
-  compdb_path = tmp_codebase / "compile_commands.json"
-  compdb_path.write_text("[\n" + compdb_path.read_text().rstrip(",\n") + "\n]\n")
-  hermetic.run(["make"], cwd=tmp_codebase, check=True)
-  assert (tmp_codebase / "compile_commands.json").exists()
+    codebase = test_dir / "test_2"
+    translation_preparation.copy_codebase(codebase, tmp_codebase)
+    # For reasons I don't understand yet, intercept-build sometimes creates an empty
+    # compile_commands.json on macOS.
+    hermetic.run(
+        ["clang", "-c", "a.c", "b.c", "main.c", "-MJ", "compile_commands.json"],
+        cwd=tmp_codebase,
+        check=True,
+    )
+    # -MJ produces a comma-terminated list; fix it to be valid JSON
+    compdb_path = tmp_codebase / "compile_commands.json"
+    compdb_path.write_text("[\n" + compdb_path.read_text().rstrip(",\n") + "\n]\n")
+    hermetic.run(["make"], cwd=tmp_codebase, check=True)
+    assert (tmp_codebase / "compile_commands.json").exists()
 
-  # Build and run the C program
-  c_prog_output = hermetic.run(
-      [str(tmp_codebase / "made_main")],
-      check=True,
-      capture_output=True,
-  )
+    # Build and run the C program
+    c_prog_output = hermetic.run(
+        [str(tmp_codebase / "made_main")],
+        check=True,
+        capture_output=True,
+    )
 
-  translation.do_translate(
+    translation.do_translate(
         root,
         tmp_codebase,
         tmp_resultsdir,
         cratename="smoke_test_2",
         guidance_path_or_literal="{}",
-  )
+    )
 
-  assert (tmp_resultsdir / "final" / "Cargo.toml").exists()
+    assert (tmp_resultsdir / "final" / "Cargo.toml").exists()
 
-  run_cargo_on_final(tmp_resultsdir / "final", ["build"])
-  rs_prog_output = run_cargo_on_final(tmp_resultsdir / "final", ["run"], capture_output=True)
+    run_cargo_on_final(tmp_resultsdir / "final", ["build"])
+    rs_prog_output = run_cargo_on_final(tmp_resultsdir / "final", ["run"], capture_output=True)
 
-  assert rs_prog_output.stdout == c_prog_output.stdout
+    assert rs_prog_output.stdout == c_prog_output.stdout
+
 
 def test_smoketest3(root, test_dir, test_tmp_dir, tmp_codebase, tmp_resultsdir):
     codebase = test_dir / "test_03"
