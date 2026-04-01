83f6386f97bbdeb013393a511e856a1f3dfcb337
Ben Karel <bkarel@aarno-labs.com>
2026-04-01 13:48:32 -0400

    Ensure that relooper-lifted decls become mutable

--- tests/smoke_tests/cmake_lone_exe/main.c
+++ tests/smoke_tests/cmake_lone_exe/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main()
+void lift_const(int argc, char** argv) {
+	// jump past initializer to trigger variable rebinding in translated code
+	if (argc == 3) { goto three_args; }
+
+	// XREF:relooper_lifted_decls_must_be_mutable
+	const int starts_with_a = *argv[0] == 'a';
+	if (starts_with_a) {
+		printf("  (arg 0 starts with 'a')\n");
+	}
+
+	three_args:
+	if (argc == 3) {
+		printf("  (argc was 3)\n");
+	}
+}
+
+int main(int argc, char** argv)
 {
   printf("Hello, Tenjin!\n");
+  lift_const(argc, argv);
   return 0;
 }
