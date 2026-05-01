6e0620918845b444be8de6cfee314c6004b57aed
Ben Karel <bkarel@aarno-labs.com>
2026-05-01 11:38:53 -0400

    Add snapshotted test showing translation of `cond ? "true" : "false"`

--- tests/snapshotted/various_unguided/snapshot/00_out/src/various_unguided.rs
+++ tests/snapshotted/various_unguided/snapshot/00_out/src/various_unguided.rs
@@ -1,5 +1,6 @@
 extern "C" {
     fn isatty(_: ::core::ffi::c_int) -> ::core::ffi::c_int;
+    fn puts(_: *const ::core::ffi::c_char) -> ::core::ffi::c_int;
 }
 pub const STDIN_FILENO: ::core::ffi::c_int = 0 as ::core::ffi::c_int;
 pub const STDOUT_FILENO: ::core::ffi::c_int = 1 as ::core::ffi::c_int;
@@ -15,4 +16,18 @@ pub unsafe extern "C" fn isatty_stderr() -> ::core::ffi::c_int {
 #[no_mangle]
 pub unsafe extern "C" fn isatty_stdin() -> ::core::ffi::c_int {
     return isatty(STDIN_FILENO);
+}
+#[no_mangle]
+pub unsafe extern "C" fn string_cond_1(mut cond: ::core::ffi::c_int) {
+    println!("{:>}", {
+        std::ffi::CStr::from_ptr(
+            (if cond != 0 {
+                b"true\0" as *const u8 as *const ::core::ffi::c_char
+            } else {
+                b"false\0" as *const u8 as *const ::core::ffi::c_char
+            }) as *const core::ffi::c_char,
+        )
+        .to_str()
+        .unwrap()
+    });
 }
--- tests/snapshotted/various_unguided/various_unguided.c
+++ tests/snapshotted/various_unguided/various_unguided.c
@@ -3,3 +3,6 @@
 int isatty_stdout() { return isatty(STDOUT_FILENO); }
 int isatty_stderr() { return isatty(STDERR_FILENO); }
 int isatty_stdin() { return isatty(STDIN_FILENO); }
+
+extern int puts(const char*);
+void string_cond_1(int cond) { puts(cond ? "true" : "false"); }
