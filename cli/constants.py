5c6e8c1bc0d6cf692b9e038dc215a4403b200032
Ben Karel <bkarel@aarno-labs.com>
2026-09-22 12:23:29 -0400

    Provision pangs @ rev-ac41d24e3

--- cli/constants.py
+++ cli/constants.py
@@ -25,7 +25,7 @@ WANT = {
     "10j-find_unsafe2-rust-toolchain": "nightly-2026-05-11",
     "10j-ast-grep": "0.40.5",
     "10j-crat": "crat-f598249c3",
-    "10j-pangs": "rev-47d4f8bab",
+    "10j-pangs": "rev-ac41d24e3",
 }
 
 XJ_GUIDANCE_FILENAME = "xj-guidance.json"
